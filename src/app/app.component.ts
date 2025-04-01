3798588f3d3f51626fc6a8d7c836bbd215b3243b
kleber7000 <kleberworldsites@gmail.com>
2025-04-01 18:58:34 -0300

    home-inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -60,5 +60,4 @@ export const routes: Routes = [
     component: SobreNosComponent
   },
 
-
 ];
